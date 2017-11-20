5622c2e0244925b8f4f4fc723a9750a7208cafb4
Andrew Yip <yipcma@gmail.com>
2017-11-20 11:47:03 +0300

    add special method __init__ the constructor and overwrite method __str__

--- classes.py
+++ classes.py
@@ -2,12 +2,26 @@ students = []
 
 
 class Student:
-    def add_student(self, name, student_id=332):
-        student = {"name": name, "student_id": student_id}
-        students.append(student)
+    
+    school_name = "Springfield Elementary"
+    
+    def __init__(self, name, student_id=332):
+        self.name = name
+        self.student_id = student_id
+        students.append(self)
 
+    def __str__(self):
+        return "Student " + self.name
+    
+    def get_name_capitalize(self):
+        return self.name.capitalize()
 
-student = Student()
-student.add_student("Mark")
+    def get_school_name(self):
+        return self.school_name
 
+mark = Student("mark")
+print(mark)
 print(students)
+
+# print class attribute
+print(Student.school_name)
