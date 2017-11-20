8ba85917a1aebb24805f3ebeec2a8ddd881fd0e3
Andrew Yip <yipcma@gmail.com>
2017-11-20 11:42:39 +0300

    first try in class with a method

---
+++ classes.py
@@ -0,0 +1,13 @@
+students = []
+
+
+class Student:
+    def add_student(self, name, student_id=332):
+        student = {"name": name, "student_id": student_id}
+        students.append(student)
+
+
+student = Student()
+student.add_student("Mark")
+
+print(students)
